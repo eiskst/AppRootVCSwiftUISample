69fff06b2c3ec6b78b8b8b80e80d9fc8a1fb49eb
Ei <eiskst@gmail.com>
2025-11-21 14:46:28 +0900

    Initial Commit

---
+++ AppRootVCSwiftUISample/AppRootVCSwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppRootVCSwiftUISampleApp.swift
+//  AppRootVCSwiftUISample
+//
+//  Created by Eisuke Sato on 2025/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AppRootVCSwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppRootVCSwiftUISampleTests/AppRootVCSwiftUISampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppRootVCSwiftUISampleTests.swift
+//  AppRootVCSwiftUISampleTests
+//
+//  Created by Eisuke Sato on 2025/11/21.
+//
+
+import Testing
+@testable import AppRootVCSwiftUISample
+
+struct AppRootVCSwiftUISampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
